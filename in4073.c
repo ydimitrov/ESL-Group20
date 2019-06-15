172d1fd06a5013ea8cf6f3322924c4de499db3c1
Yordan Dimitrov <y.dimitrov.14@gmail.com>
2019-06-15 13:44:36 +0200

    Clearing issues

--- in4073.c
+++ in4073.c
@@ -52,12 +52,13 @@ int main(void)
 			adc_request_sample();
 			// read_baro();
 
-			// printf("%10ld | ", get_time_us());
-			// printf("%3d %3d %3d %3d | ", ae[0], ae[1], ae[2], ae[3]);
-			// printf("%d | ", mode);
-			// printf("%6d %6d %6d | ", phi, theta, psi);
-			// printf("%6d %6d %6d | ", sp, sq, sr);
-			// printf("%4d | %4ld | %6ld", bat_volt, temperature, pressure);
+			printf("%10ld | ", get_time_us());
+			printf("%3d %3d %3d %3d | ", ae[0], ae[1], ae[2], ae[3]);
+			printf("%d | ", mode);
+			printf("%6d %6d %6d | ", phi, theta, psi);
+			printf("%6d %6d %6d | ", sp, sq, sr);
+			printf("%4d | %4ld | %4ld |", bat_volt, temperature, pressure);
+			printf("P = %3d | P1 = %3d | P2 = %3d |\n ", P, P1, P2);
 
 			clear_timer_flag();
 		}
@@ -68,7 +69,7 @@ int main(void)
 		{
 			get_dmp_data();
 			run_filters_and_control();
-			commStatus();
+			// commStatus();
 		}	
 	}	
 
